377355665d5842cca948823eaa46185cfb826bbc
3311453797 <3311453797@qq.com>
2019-01-20 13:49:31 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>服务器解析生成工具</title>
+<link rel="stylesheet" href="static/css/layui.css">
+<script src="static/layui.js"></script>
+</head>
+<body class="layui-layout-body layui-bg-gray">
+<div class="layui-layout layui-layout-admin">
+  <div class="layui-header">
+    <div class="layui-logo">服务器解析生成工具</div>
+  </div>
+<div class="layui-footer">
+    &copy; 2015-<?php echo date("Y")?> <a href="https://cloud.matrixt.cn">矩阵云</a> <a href="https://dev.tencent.com/u/KJWL/p/SRVT">本程序已开源</a>
+  </div>
+</div>
+</body>
+</html>
