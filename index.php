2af063ec1fd84dd1ab4bf18f0bb5a59afe3d8503
3311453797 <3311453797@qq.com>
2019-01-20 17:08:18 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- index.php
+++ index.php
@@ -7,14 +7,26 @@
 <link rel="stylesheet" href="static/css/layui.css">
 <script src="static/layui.js"></script>
 </head>
-<body class="layui-layout-body layui-bg-gray">
-<div class="layui-layout layui-layout-admin">
-  <div class="layui-header">
-    <div class="layui-logo">服务器解析生成工具</div>
-  </div>
-<div class="layui-footer">
-    &copy; 2015-<?php echo date("Y")?> <a href="https://cloud.matrixt.cn">矩阵云</a> <a href="https://dev.tencent.com/u/KJWL/p/SRVT">本程序已开源</a>
+<body>
+<form class="layui-form layui-form-pane" action="backend.php" method="get">
+<div class="layui-form-item">
+  <label class="layui-form-label">IP</label>
+  <div class="layui-input-block">
+    <input type="text" name="ip" required lay-verify="required" placeholder="请输入服务器外网IP" autocomplete="off" class="layui-input"></input>
   </div>
 </div>
+<div class="layui-form-item">
+  <label class="layui-form-label">域名前缀</label>
+  <div class="layui-input-block">
+    <input type="text" name="domainprefix" required lay-verify="required" placeholder="请输入你的域名前缀(prefix.xxx.com)" autocomplete="off" class="layui-input">    
+  </div>
+</div>
+<div class="layui-form-item">
+  <label class="layui-form-label">使用SRV</label>
+  <div class="layui-input-block">
+    原始表单元素区域
+  </div>
+</div>
+</form>
 </body>
 </html>
